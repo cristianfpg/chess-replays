536e20217a25af4bb986844e538c6286ef8775de
Cristian Pacheco <cristianfpg@gmail.com>
2025-11-08 22:46:03 -0500

    Refactor backend: add Hero model and endpoints; update requirements and database initialization

--- back/main.py
+++ back/main.py
@@ -2,12 +2,39 @@ from typing import Union
 
 from fastapi import FastAPI
 
-app = FastAPI()
+from sqlmodel import Field, Session, SQLModel, create_engine
 
+app = FastAPI()
+engine = create_engine("sqlite:///database.db")
+SQLModel.metadata.create_all(engine)
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/all_heroes")
+def read_hero():
+    with Session(engine) as session:
+        heroes = session.query(Hero).all()
+        return heroes
+
+@app.get("/test")
+def read_test():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        session.commit()
+        return {"Test": "Success"}
 
 
 @app.get("/items/{item_id}")
